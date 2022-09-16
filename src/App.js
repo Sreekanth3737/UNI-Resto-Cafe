cb1560b0407a1f7697af55fe2e7333dff0cf7089
SREEKANTH PS <sreekanth3265@gmail.com>
2022-09-16 10:06:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "postcss";
+import { useState } from "react";
+import CategoryDishes from "./Components/CategoryDishes/CategoryDishes";
+import MenuList from "./Components/MenuList/MenuList";
+import Navbar from "./Components/Navigation/Navbar";
+import Cart from "./context/CartCotext";
+
+
+
 
 function App() {
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+    <Cart>
+
+      <Navbar    />
+      
+      <MenuList />
+      <CategoryDishes />
+      
+    </Cart>
+    </>
   );
 }
 
---
+++ src/redux/store/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit'
+import menuReducer from '../slices/menuSlices'
+
+const store=configureStore({
+    reducer:{
+        menuItem:menuReducer,
+        
+    },
+});
+export default store;
